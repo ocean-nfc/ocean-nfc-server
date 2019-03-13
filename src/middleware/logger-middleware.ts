6219f3c3267ae558d53d52df25fbef9edc780317
Given <u15135421@tuks.co.za>
2019-03-13 16:05:46 +0200

    Update logger-middleware.ts
    Updated to call addToLog so log item can saved to database

--- src/middleware/logger-middleware.ts
+++ src/middleware/logger-middleware.ts
@@ -19,7 +19,7 @@ export const loggerMiddleware: express.RequestHandler = (req: express.Request, r
 
   console.log(logItem);
   // TODO: Save log item to log database
-  //addToLog() 
+  addToLog(logItem); //Calling the function to add logItem to the database
 
   next();
 }
