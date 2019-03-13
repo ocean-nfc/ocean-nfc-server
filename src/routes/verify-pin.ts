2125d897a8feea688e207557cd90d4886d061389
u16262949 <u16262949@tuks.co.za>
2019-03-13 20:50:58 +0200

    check pin api endpoint

--- src/routes/verify-pin.ts
+++ src/routes/verify-pin.ts
@@ -1,19 +1,29 @@
-import { NotAllParamsSuppliedException } from './../exceptions';
+import { Database } from './../classes/database';
+import { NotAllParamsSuppliedException, Exception } from './../exceptions';
 import * as express from "express";
 import { ClientIdNotFoundException } from "../exceptions";
 
 export const verifyPin: express.RequestHandler = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const userId = req.query.userId;
+  const clientId = req.query.clientId;
   const pin = req.query.pin;
-  if (!userId || !pin) {
+  if (!clientId || !pin) {
     return next(new NotAllParamsSuppliedException());
   }
 
   // if the user doesn't exist next(new ClientNotFoundException())
 
-  const isPinValid = false; // actually perform validation
+  const db = Database.getInstance();
 
-  res.json({
-    valid: isPinValid,
-  });
+  try {
+    const users = await db.all("SELECT * FROM db WHERE (clientId=?)", [clientId]);
+    if (users.length === 0) {
+      return next(new ClientIdNotFoundException());
+    }
+    res.json({
+      valid: users[0].pin == pin
+    });
+  } catch (e) {
+    console.error("error:", e);
+    return next(new Exception());
+  }
 }
