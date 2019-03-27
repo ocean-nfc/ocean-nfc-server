960120125611c1dabac8b6efe136ef572287c0e4
Dewald <u15030378@tuks.co.za>
2019-03-27 23:38:37 +0200

    Should work, could not get testing to work
    The code should work, just had issues with the testing, need to figure out how to get TS to import correctly

--- src/routes/verify-pin.ts
+++ src/routes/verify-pin.ts
@@ -2,26 +2,31 @@ import { Database } from './../classes/database';
 import { NotAllParamsSuppliedException, Exception } from './../exceptions';
 import * as express from "express";
 import { ClientIdNotFoundException } from "../exceptions";
+import {AuthException} from "../exceptions";
 
 export const verifyPin: express.RequestHandler = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const clientId = req.query.clientId;
+  const cardId = req.query.cardId;
   const pin = req.query.pin;
-  if (!clientId || !pin) {
+  if (!cardID || !pin) {
     return next(new NotAllParamsSuppliedException());
   }
 
-  // if the user doesn't exist next(new ClientNotFoundException())
 
   const db = Database.getInstance();
 
   try {
-    const users = await db.all("SELECT * FROM db WHERE (clientId=?)", [clientId]);
+    const users = await db.all("SELECT * FROM db WHERE (cardNumber=?)", [cardId]);
     if (users.length === 0) {
-      return next(new ClientIdNotFoundException());
+      return next(new AuthException());//card not found
     }
-    res.json({
-      valid: users[0].pin == pin
-    });
+    if (users[0].pin == pin)
+    {
+      res.json({users[0].clientId, "AUTHENTICATION SUCCESS"});//card found, pin matches
+    }
+    else
+    {
+      res.json({users[0].clientId, "AUTHENTICATION FAILURE"});//card found, pin incorrect
+    }
   } catch (e) {
     console.error("error:", e);
     return next(new Exception());
