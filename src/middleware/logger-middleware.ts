1e734a8f66b2f124f371e68e24b55bd1ab1d7ce4
Given <u15135421@tuks.co.za>
2019-03-29 14:14:16 +0200

    Update logger-middleware.ts
    The pin is now masked before making a log (this is for security purposes).

--- src/middleware/logger-middleware.ts
+++ src/middleware/logger-middleware.ts
@@ -9,6 +9,16 @@ import * as express from "express";
  */
 export const loggerMiddleware: express.RequestHandler = (req: express.Request, res: express.Response, next: express.NextFunction) => {
   res.on("finish", function() {
+
+    var index = req.originalUrl.indexOf("pin=");
+    if(index > -1) {
+      req.originalUrl = req.originalUrl.substring(0,index);
+      req.originalUrl = req.originalUrl.concat("pin=undifined"); //This is to mask the pin (security purposes)
+    }
+
+    if(req.query['pin'] != null)
+      req.query['pin'] = "undifined"; //This is to mask the pin (security purposes)
+
     const logItem = [
       Math.round(Date.now() / 1000),
       res.statusCode,
