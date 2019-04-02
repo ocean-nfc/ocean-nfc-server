a543dad14a809dfe1f9e22d22eeb43e3543ee550
u16262949 <u16262949@tuks.co.za>
2019-04-02 16:50:51 +0200

    Updated logging to not lose logs while sending to server.

--- src/classes/log.ts
+++ src/classes/log.ts
@@ -11,8 +11,11 @@ export class Log {
     return Log.instance;
   }
 
-  private constructor() {}
   private file = "./log.json";
+
+  private isSendingFile = false;
+
+  private constructor() {}
 
   /**
    * Initialises the json log file
@@ -28,10 +31,10 @@ export class Log {
    * @param logData
    */
   public addLogItem(logData) {
-    if(!fs.existsSync("./log.json")) {
+    if (!fs.existsSync("./log.json")) {
       this.initialiseLogFile();
     }
-    
+
     var fileObj = jsonfile.readFileSync(this.file);
 
     const log = {
@@ -43,42 +46,107 @@ export class Log {
       ip: logData[5]
     };
 
-    fileObj['data'].push(log);
+    fileObj["data"].push(log);
     this.writeToLogFile(fileObj);
     console.log("Log Item was appended to file.");
 
-    if(fileObj['data'].length >= 100){
+    if (fileObj["data"].length >= 100 && !this.isSendingFile) {
       this.sendLogFile(fileObj);
     }
   }
 
   /**
    * Writes the logs into the log file
-   * @param jsonObj 
+   * @param jsonObj
    */
   private writeToLogFile(jsonObj) {
-    jsonfile.writeFileSync(this.file, jsonObj, { flag: "w"} , function(err) {
+    jsonfile.writeFileSync(this.file, jsonObj, { flag: "w" }, function(err) {
       if (err) console.error(err);
-    });    
+    });
   }
 
   /**
    * Sends the log file data to the reporting subsystem
-   * @param fileData 
+   * @param fileData
    */
   private sendLogFile(fileData) {
-    axios.default.post("https://fnbreports-6455.nodechef.com/api", { system: "CRDS", data: JSON.stringify(fileData['data'])})
-    .then((res) => {
-      console.log("Log file has be sent to reporting statusCode: "+res.status);
-      console.log(res.data);
-      fs.unlinkSync("./log.json");
-    })
-    .catch((err) => {
-      console.error(err.status+" "+err.data);
-    });
+    this.isSendingFile = true;
+    const lastLogIndex = fileData.data.length;
+
+    axios.default
+      .post("https://fnbreports-6455.nodechef.com/api", {
+        system: "CRDS",
+        data: JSON.stringify(fileData.data)
+      })
+      .then(res => {
+        console.log(
+          "Log file has be sent to reporting statusCode: " + res.status
+        );
+        console.log(res.data);
+
+        // remove the logs sent from the file
+        const file: {data: any[], system: string} = jsonfile.readFileSync(this.file);
+        file.data = file.data.slice(lastLogIndex);
+        this.writeToLogFile(file);
+        this.isSendingFile = false;
+      })
+      .catch(err => {
+        console.error(err.status + " " + err.data);
+        this.isSendingFile = false;
+      });
   }
 
   public async reset() {
     await this.initialiseLogFile();
+  }
+}
+
+class Lock {
+  private isLocked = false;
+  private waiting = [];
+  private fn: (...parameters) => Promise<void>;
+
+  costructor(lockedFn: (...parameters) => Promise<void>) {
+    this.fn = lockedFn;
+  }
+
+  public request(...parameters): Promise<any> {
+    return new Promise(async resolve => {
+      const waiter = new ExternalPromise().getPromise();
+
+      if (this.isLocked) {
+        this.waiting.push(waiter);
+        await waiter;
+      }
+
+      this.isLocked = true;
+      await this.fn(...parameters);
+      this.isLocked = false;
+
+      if (this.waiting.length) {
+        this.waiting[0].resolve();
+      }
+
+      resolve();
+    });
+  }
+}
+
+class ExternalPromise {
+  private resolver;
+  private promise;
+
+  constructor() {
+    this.promise = new Promise(resolve => {
+      this.resolver = resolve;
+    });
+  }
+
+  public resolve() {
+    this.resolver();
+  }
+
+  public getPromise() {
+    return this.promise;
   }
 }
