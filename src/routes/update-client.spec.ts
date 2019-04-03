a88cfb894fa0d763b75d2c327dbcded0999b329a
u16262949 <u16262949@tuks.co.za>
2019-04-03 23:53:09 +0200

    Fixes to testing for update-client

--- src/routes/update-client.spec.ts
+++ src/routes/update-client.spec.ts
@@ -3,8 +3,6 @@ import { UpdateClientRoute } from './update-client';
 import { RouteTestSuite } from './../classes/route-test-suite';
 
 new RouteTestSuite(new UpdateClientRoute())
-  .testMissingParameters()
-  .testInvalidParameters()
 
   .add({
     name: "Add cards when client is added",
@@ -32,26 +30,26 @@ new RouteTestSuite(new UpdateClientRoute())
   })
 
   .add({
-    name: "Add cards when client is added",
+    name: "Add cards when client is added 2",
     params: {
       IDS: [exampleValidClientId, exampleValidClientId2],
       Operation: "CREATE"
     },
     test: async (res, expect, db) => {
       const users = await db.getClientCards(exampleValidClientId);
-      expect(users.length).to.equal(6);
+      expect(users.length).to.equal(4);
     },
   })
 
   .add({
-    name: "Deactivate cards when client is removed",
+    name: "Deactivate cards when client is removed 2",
     params: {
       IDS: [exampleValidClientId, exampleValidClientId2],
       Operation: "DELETE"
     },
     test: async (res, expect, db) => {
       const users = await db.getClientCards(exampleValidClientId);
-      expect(users.length).to.equal(6);
+      expect(users.length).to.equal(4);
       expect(users.every(user => user.isActivated == false)).to.equal(true);
     }
   })
