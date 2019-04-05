a6bfa1405909599186c1efc267242a5e0233abf6
u16262949 <u16262949@tuks.co.za>
2019-04-05 08:02:47 +0200

    Reduce impact on CIS by: 1. Only requesting sync in prod mode 2. Set sync interval to 20 minutes not 10

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
+import { config } from './src/config';
 import { CISSubscriber } from './src/classes/cis-subscriber';
 import { Server } from "./src/server";
 
 (async () => {
   await new Server().start();
-  CISSubscriber.subscribe();
+
+  if (!config.developmentMode)
+    CISSubscriber.subscribe();
 })();
